4434dc91e07ab32961ef25bfe41b37e9340f47b1
sun30812 <sun30812@naver.com>
2022-02-01 17:16:16 +0900

    문제 수정: 시스템 종료가 동작 안하는 문제 수정
    * shutdown.exe의 잘못된 옵션 수정
    * 기존 함수를 사용하도록 수정

--- sn30pe-launcher.ps1
+++ sn30pe-launcher.ps1
@@ -1,6 +1,6 @@
 $version = "0.1b"
 $main = [System.Windows.Forms.Form]::new()
-$main.Size = [System.Drawing.Size]::new(700,400)
+$main.Size = [System.Drawing.Size]::new(700, 400)
 $main.Text = "앱 런처"
 
 function versionInfoClick ($object, $eventArg) {
@@ -16,9 +16,10 @@ function systemManage ($object, $eventArg, [bool]$isRestart) {
         if ([System.Windows.Forms.MessageBox]::Show("시스템을 다시시작하시겠습니까?", "다시시작", [System.Windows.Forms.MessageBoxButtons]::OKCancel) -eq "OK") {
             shutdown.exe /r /t 0
         }
-    } else {
+    }
+    else {
         if ([System.Windows.Forms.MessageBox]::Show("시스템을 종료하시겠습니까?", "시스템 종료", [System.Windows.Forms.MessageBoxButtons]::OKCancel) -eq "OK") {
-            shutdown.exe /t 0
+            shutdown.exe /s /t 0
         } 
     }
 }
@@ -31,23 +32,21 @@ $appsView = [System.Windows.Forms.ListView]::new()
 $infoMenu.Text = "정보"
 $systemMenu.Text = "시스템"
 $appsView.Size = [System.Drawing.Size]::new(600, 300)
-$appsView.Location = [System.Drawing.Point]::new(30,30)
+$appsView.Location = [System.Drawing.Point]::new(30, 30)
 $notepad = [System.Windows.Forms.ListViewItem]::new("메모장")
 $notepad.Tag = "notepad"
 $appsView.Items.Add($notepad)
-$appsView.Add_DoubleClick({appsClick $appsView, $e})
+$appsView.Add_DoubleClick({ appsClick $appsView, $e })
 $versionInfo = [System.Windows.Forms.ToolStripMenuItem]::new()
 $shutdown = [System.Windows.Forms.ToolStripMenuItem]::new()
 $reboot = [System.Windows.Forms.ToolStripMenuItem]::new()
 $versionInfo.Text = "버전 정보: ${version}"
 $shutdown.Text = "종료"
-$shutdown.Add_Click({systemManage})
+$shutdown.Add_Click({ systemManage })
 $reboot.Text = "다시시작"
-$reboot.Add_Click({systemManage -isRestart $true})
-$versionInfo.Add_Click({versionInfoClick})
-$shutdown.Add_Click({
-    shutdown.exe /t 0
-})
+$reboot.Add_Click({ systemManage -isRestart $true })
+$versionInfo.Add_Click({ versionInfoClick })
+$shutdown.Add_Click({ systemManage -isRestart $false })
 $infoMenu.DropDownItems.Add($versionInfo)
 $systemMenu.DropDownItems.AddRange(@($shutdown, $reboot))
 $mainMenu.Items.Add($infoMenu)
